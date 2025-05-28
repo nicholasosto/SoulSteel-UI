eccd89f6db98b6d551ea4c1eaa3d5855fbfe1697
Nicholas Osto <nicholas.osto@gmail.com>
2025-05-28 19:29:53 -0400

    chore: remove unnecessary blank lines in EquipmentState.ts

--- src/shared/FusionUI/states/EquipmentState.ts
+++ src/shared/FusionUI/states/EquipmentState.ts
@@ -2,8 +2,6 @@ import Fusion, { Computed } from "@rbxts/fusion";
 import { Players } from "@rbxts/services";
 const LocalPlayer = Players.LocalPlayer;
 
-
-
 export class EquipmentState {
 	private static Initialized() {}
 }
